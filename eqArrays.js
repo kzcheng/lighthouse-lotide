fc15d9a92a380248a067dde6951e6ff93c29b308
kzcheng <kevinzifancheng@gmail.com>
2020-01-07 23:37:55 +0000

    Finished eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -4,13 +4,19 @@ const eqArrays = function(a1, a2) {
   if (a1.length !== a2.length) {
     return false;
   }
-  
 
+  for (let i = 0; i < a1.length; i++) {
+    if (a1[i] !== a2[i]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // [TEST CODE]
-eqArrays([1, 2, 3], [1, 2, 3]); // true
-eqArrays([1, 2, 3], [3, 2, 1]); // false
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // false
