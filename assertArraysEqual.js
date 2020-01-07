06591920e8d23412d1e3a151b015117e871ff7cb
kzcheng <kevinzifancheng@gmail.com>
2020-01-07 23:41:49 +0000

    Created and completed assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+// [MANUAL IMPORT]
+const eqArrays = function(a1, a2) {
+  // Check
+  if (a1.length !== a2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < a1.length; i++) {
+    if (a1[i] !== a2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// [FUNCTION IMPLEMENTATION]
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
+    return true;
+  } else {
+    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
+    return false;
+  }
+};
+
+// [TEST CODE]
