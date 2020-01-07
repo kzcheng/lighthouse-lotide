82f2451158f8d137a87324582796d61d85bdc3e7
kzcheng <kevinzifancheng@gmail.com>
2020-01-06 19:49:14 -0800

    Added Template Literals to simplify outputting

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion Passed: [" + actual + "] === [" + expected + "]");
+    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
     return true;
   } else {
-    console.log("Assertion Failed: [" + actual + "] !== [" + expected + "]");
+    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
     return false;
   }
 };
