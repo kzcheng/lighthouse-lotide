0a5f65f7083f7c77cecc7c8c3e90910180839046
kzcheng <kevinzifancheng@gmail.com>
2020-01-08 12:50:18 -0800

    Added disable for eslint

--- countOnly.js
+++ countOnly.js
@@ -1,7 +1,11 @@
 // [Shortened Commonly Used Functions]
+// eslint-disable-next-line no-unused-vars
 const l = console.log.bind(console);
+// eslint-disable-next-line no-unused-vars
 const d = console.debug.bind(console);
+// eslint-disable-next-line no-unused-vars
 const w = console.warn.bind(console);
+// eslint-disable-next-line no-unused-vars
 const e = console.error.bind(console);
 
 // [Function Implementation]
