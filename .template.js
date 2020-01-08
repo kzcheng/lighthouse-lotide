3d2f373ab3f3d1874d51448492df51065c5416e9
kzcheng <kevinzifancheng@gmail.com>
2020-01-08 13:54:17 -0800

    Created a template for new files, also created findKeyByValue.js

---
+++ .template.js
@@ -0,0 +1,17 @@
+// [Shortened Commonly Used Functions]
+// eslint-disable-next-line no-unused-vars
+const l = console.log.bind(console);
+// eslint-disable-next-line no-unused-vars
+const d = console.debug.bind(console);
+// eslint-disable-next-line no-unused-vars
+const w = console.warn.bind(console);
+// eslint-disable-next-line no-unused-vars
+const e = console.error.bind(console);
+
+
+// [Function Implementation]
+const name = function() {
+};
+
+// [Testing Code]
+l(name());
