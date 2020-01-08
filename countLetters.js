bb17e7a3b07f97fb4da7607ee2bdd4f004159da8
kzcheng <kevinzifancheng@gmail.com>
2020-01-08 12:53:30 -0800

    Created countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,15 @@
+// [Shortened Commonly Used Functions]
+// eslint-disable-next-line no-unused-vars
+const l = console.log.bind(console);
+// eslint-disable-next-line no-unused-vars
+const d = console.debug.bind(console);
+// eslint-disable-next-line no-unused-vars
+const w = console.warn.bind(console);
+// eslint-disable-next-line no-unused-vars
+const e = console.error.bind(console);
+
+// [Function Implementation]
+const countLetters = function() {
+};
+
+// [Testing Code]
