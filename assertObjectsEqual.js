aeda9c8cc99c72086b5b5592769338aba30ff1b7
kzcheng <kevinzifancheng@gmail.com>
2020-01-09 09:20:38 -0800

    Created assertObjectsEqual.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,14 @@
+// [Shortened Commonly Used Functions]
+// eslint-disable-next-line no-unused-vars
+const l = console.log.bind(console);
+
+// [Manual Imports]
+// Empty
+
+
+// [Function Implementation]
+const name = function() {
+};
+
+// [Testing Code]
+l(name());
