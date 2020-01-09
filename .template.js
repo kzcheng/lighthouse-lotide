ba7a18fe52aff57a59291c371519333f5963ef1a
kzcheng <kevinzifancheng@gmail.com>
2020-01-08 18:58:18 -0800

    Edited template

--- .template.js
+++ .template.js
@@ -1,12 +1,9 @@
 // [Shortened Commonly Used Functions]
 // eslint-disable-next-line no-unused-vars
 const l = console.log.bind(console);
-// eslint-disable-next-line no-unused-vars
-const d = console.debug.bind(console);
-// eslint-disable-next-line no-unused-vars
-const w = console.warn.bind(console);
-// eslint-disable-next-line no-unused-vars
-const e = console.error.bind(console);
+
+// [Manual Imports]
+// Empty
 
 
 // [Function Implementation]
