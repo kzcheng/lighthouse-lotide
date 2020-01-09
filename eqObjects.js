23f37a88699705c86bac16e7270389384655b5c2
kzcheng <kevinzifancheng@gmail.com>
2020-01-08 19:50:56 -0800

    Implemented array comparison

--- eqObjects.js
+++ eqObjects.js
@@ -4,6 +4,8 @@ const l = console.log.bind(console);
 
 // [Manual Imports]
 const eqArrays = function(a1, a2) {
+  // l(a1, a2);
+
   // Check
   if (a1.length !== a2.length) {
     return false;
@@ -27,7 +29,10 @@ const eqObjects = function(obj1, obj2) {
   
   for (const key in obj1) {
     if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
-      
+      // l(obj1[key], obj2[key]);
+      if (!eqArrays(obj1[key], obj2[key])) {
+        return false;
+      }
     } else if (obj1[key] !== obj2[key]) {
       return false;
     }
