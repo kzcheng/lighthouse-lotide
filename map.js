3e4b368347ae0c3480a149ce59e368b140468c95
kzcheng <kevinzifancheng@gmail.com>
2020-01-09 15:48:17 -0800

    Written a template in

---
+++ map.js
@@ -0,0 +1,14 @@
+// [Shortened Commonly Used Functions]
+// eslint-disable-next-line no-unused-vars
+const l = console.log.bind(console);
+
+// [Manual Imports]
+// Empty
+
+
+// [Function Implementation]
+const map = function() {
+};
+
+// [Testing Code]
+l(map());
