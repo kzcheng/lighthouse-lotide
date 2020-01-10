edf3e6038ab8092720d5281fc847e68a427f6dce
kzcheng <kevinzifancheng@gmail.com>
2020-01-09 16:31:38 -0800

    Created and completed takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,30 @@
+// [Shortened Commonly Used Functions]
+// eslint-disable-next-line no-unused-vars
+const l = console.log.bind(console);
+
+// [Manual Imports]
+// Empty
+
+
+// [Function Implementation]
+const takeUntil = function(array, callback) {
+  let returnArray = [];
+  for (const element of array) {
+    if (callback(element)) {
+      return returnArray;
+    }
+    returnArray.push(element);
+  }
+  return returnArray;
+};
+
+// [Testing Code]
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
