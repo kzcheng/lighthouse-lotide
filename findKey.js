43da6e7953d9893510ac05e3ff7e98539e1137ab
kzcheng <kevinzifancheng@gmail.com>
2020-01-09 16:43:49 -0800

    Created and Finished findKey.js

---
+++ findKey.js
@@ -0,0 +1,27 @@
+// [Shortened Commonly Used Functions]
+// eslint-disable-next-line no-unused-vars
+const l = console.log.bind(console);
+
+// [Manual Imports]
+// Empty
+
+
+// [Function Implementation]
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// [Testing Code]
+l(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
