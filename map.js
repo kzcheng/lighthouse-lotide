a535d001823229640480541abe8a9f463d422f2d
kzcheng <kevinzifancheng@gmail.com>
2020-01-09 16:31:24 -0800

    Completed map.js

--- map.js
+++ map.js
@@ -7,8 +7,16 @@ const l = console.log.bind(console);
 
 
 // [Function Implementation]
-const map = function() {
+const map = function(array, callback) {
+  let returnArray = [];
+  for (const element of array) {
+    returnArray.push(callback(element));
+  }
+  return returnArray;
 };
 
 // [Testing Code]
-l(map());
+const words = ["ground", "control", "to", "major", "tom"];
+console.log(map(words, name => name.length));
+const results1 = map(words, word => word[0]);
+console.log(results1);
