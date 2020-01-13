074cd1fe8aaac3f698713e507bd465799a85ea94
kzcheng <kevinzifancheng@gmail.com>
2020-01-13 12:54:24 -0800

    small edit

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,9 @@
-// [Requirements]
+// [Imports]
 // eslint-disable-next-line no-unused-vars
 const assertEqual = require('../assertEqual');
 // eslint-disable-next-line no-unused-vars
 const head = require('../head');
 
+// [Testing Code]
 assertEqual(head([1,2,3]), 1);
 assertEqual(head([]), undefined);
