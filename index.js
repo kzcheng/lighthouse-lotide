c0d5229ba33c22d9dd252e55328ac3f5bb3d4b4f
kzcheng <kevinzifancheng@gmail.com>
2020-01-13 15:30:36 -0800

    Created index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// [Imports]
+const head    = require('./head');
+const tail    = require('./tail');
+const middle  = require('./middle');
+
+// [Export]
+module.exports = {
+  head,
+  tail,
+  middle
+};
