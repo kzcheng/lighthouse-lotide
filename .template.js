7dec3585fb347f6837b51d1c254449c1bf1f9e26
kzcheng <kevinzifancheng@gmail.com>
2020-01-13 12:34:29 -0800

    Updated template

--- .template.js
+++ .template.js
@@ -1,3 +1,7 @@
+// [Requirements]
+// eslint-disable-next-line no-unused-vars
+const assertEqual = require('./assertEqual');
+
 // [Shortened Commonly Used Functions]
 // eslint-disable-next-line no-unused-vars
 const l = console.log.bind(console);
