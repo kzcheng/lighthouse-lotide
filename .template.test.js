7ea8bba32621006f33bade78150001d07832b5b3
kzcheng <kevinzifancheng@gmail.com>
2020-01-13 13:01:58 -0800

    edit

--- .template.test.js
+++ .template.test.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-unused-vars
 const assertEqual = require('../assertEqual');
 // eslint-disable-next-line no-unused-vars
-const head = require('../name');
+const name = require('../name');
 
 // [Testing Code]
 assertEqual(name, undefined);
