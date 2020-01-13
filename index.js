8579999a621405ace96b2ddb08bc9c6f5ad839bd
kzcheng <kevinzifancheng@gmail.com>
2020-01-13 15:44:32 -0800

    Added reference to all files

--- index.js
+++ index.js
@@ -2,10 +2,39 @@
 const head    = require('./head');
 const tail    = require('./tail');
 const middle  = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const assertEqual = require('./assertEqual');
+const countLetters = require('./countLetters');
+const eqArrays = require('./eqArrays');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
 
 // [Export]
 module.exports = {
   head,
   tail,
-  middle
+  middle,
+  assertArraysEqual,
+  assertObjectsEqual,
+  countOnly,
+  eqObjects,
+  findKey,
+  letterPositions,
+  map,
+  assertEqual,
+  countLetters,
+  eqArrays,
+  findKeyByValue,
+  flatten,
+  takeUntil,
+  without
 };
